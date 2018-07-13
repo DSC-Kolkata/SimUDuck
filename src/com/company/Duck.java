3c87ff29c5e76bd911d65c429311540e8ed8c502
Raveesh Agarwal <raveeshr503@gmail.com>
2018-07-13 13:39:38 +0530

    Branch ready

--- src/com/company/Duck.java
+++ src/com/company/Duck.java
@@ -12,4 +12,9 @@ public class Duck {
     void quack(){
         System.out.println("I am a Duck and Quack Quack Quack Quack");
     }
+
+//    //TODO: 03-Make the Ducks Fly
+//    void fly(){
+//        System.out.println("Flap Flap FLap FLap FLap");
+//    }
 }
---
+++ src/com/company/WoodenDuck.java
@@ -0,0 +1,9 @@
+package com.company;
+
+////TODO: 01-Implement a wooden Duck
+//public class WoodenDuck extends Duck{
+//    @Override
+//    void quack() {
+//        System.out.println("I am a wooden duck so no quack");
+//    }
+//}
